ef7d0b502f59b06795dc7eb1add714dba310b85b
rendrakb <rendrakb@gmail.com>
2025-05-29 05:11:55 +0700

    4

--- meditation.py
+++ meditation.py
@@ -3,7 +3,7 @@ import time
 
 try:
     import tkinter as tk
-    from tkinter import ttk, font, messagebox
+    from tkinter import ttk
 
 except (ModuleNotFoundError, ImportError):
     print(
@@ -17,7 +17,7 @@ class MeditationInterface:
     start_button: ttk.Button
     stop_button: ttk.Button
     pause_button: ttk.Button
-    time_button: ttk.Button
+    time_slider: ttk.Scale
     progress_bar: ttk.Progressbar
 
     def __init__ (self, root: tk.Tk):
@@ -33,26 +33,40 @@ class LayoutManager:
         
     def init_layout(self):
         self._buttons_frame()
+        self._time_slider_frame()
         self._progress_bar_frame()
 
     def _buttons_frame(self):
         frame = ttk.Frame(self.root)
-        frame.pack(pady=10)
+        frame.pack(pady=2)
 
         self.interface.start_button = ttk.Button(frame, text="Start")
         self.interface.stop_button = ttk.Button(frame, text="Stop")
         self.interface.pause_button = ttk.Button(frame, text="Pause")
 
-        self.interface.start_button.pack(side="left", padx=5)
-        self.interface.stop_button.pack(side="left", padx=5)
-        self.interface.pause_button.pack(side="left", padx=5)
+        self.interface.start_button.pack(side="left", padx=1)
+        self.interface.stop_button.pack(side="left", padx=1)
+        self.interface.pause_button.pack(side="left", padx=1)
+
+    def _time_slider_frame(self):
+        frame = ttk.Frame(self.root)
+        frame.pack(pady=2)
+
+        self.interface.time_label = ttk.Label(frame, text="Time: 1 min")
+        self.interface.time_label.pack(pady=2)
+
+        self.interface.time_slider = ttk.Scale(frame, from_=1, to=30, orient="horizontal", length=225, command=self._update_time_label)
+        self.interface.time_slider.pack(pady=2)
+
+    def _update_time_label(self, value):
+        self.interface.time_label.config(text=f"Time: {int(float(value))} min")
 
     def _progress_bar_frame(self):
         frame = ttk.Frame(self.root)
-        frame.pack(pady=10)
+        frame.pack(pady=2)
 
-        self.interface.progress_bar = ttk.Progressbar(frame, orient="horizontal", length=200, mode="determinate")
-        self.interface.progress_bar.pack(pady=10)
+        self.interface.progress_bar = ttk.Progressbar(frame, orient="horizontal", length=225, mode="determinate")
+        self.interface.progress_bar.pack(pady=2)
 
 def run():
     root = tk.Tk()
@@ -60,7 +74,7 @@ def run():
     root.title("Meditation")
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-    root.geometry(f"400x300+{(screen_width - 400) // 2}+{(screen_height - 300) // 2}")
+    root.geometry(f"300x125+{(screen_width - 300) // 2}+{(screen_height - 125) // 2}")
 
     app = MeditationInterface(root)
 
